16f36bdb38e4611131552d6b1ce9d9da186cfcfc
Mohammed S. Ali <iraqrev@gmail.com>
2020-04-15 02:03:13 +0300

    --player details minimized hidden state fixed on pre lollipop also

---
+++ exoplayerawesome/src/main/java/com/appchief/msa/MLExts.kt
@@ -0,0 +1,19 @@
+package com.appchief.msa
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.updateLayoutParams
+import com.appchief.msa.exoplayerawesome.DpToPx
+import kotlinx.android.synthetic.main.appchief_floating_player.view.*
+
+class MLExts @JvmOverloads constructor(
+	 context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+	 fun setTopy(newMarginTop: Int) {
+		  detailsView?.updateLayoutParams<ConstraintLayout.LayoutParams> {
+			   topMargin = newMarginTop.DpToPx()
+		  }
+	 }
+}
